8da0cd87d1393c5fb7ebdb699a18586f6b0cbc49
SabbirSami <smd71430@gmail.com>
2022-06-09 16:09:49 +0600

    navbar added

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderNav from "./HeaderNav";
+
+const Home = () => {
+    return (
+        <div>
+            <HeaderNav />
+        </div>
+    );
+};
+
+export default Home;
