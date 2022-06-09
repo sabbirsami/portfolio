60df9c85dbe4f3891ceed32b3bcc3dc3d2ffd5e1
SabbirSami <smd71430@gmail.com>
2022-06-09 18:28:29 +0600

    hero section done

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -27,16 +27,17 @@ const Hero = () => {
                             </div>
                             <div className="py-lg-4">
                                 <p className="pb-lg-4 text-muted">
-                                    <small>BIOGRAPHY</small>
+                                    <small>CONTACT</small>
                                 </p>
+                                <p className="p-0 m-0">Dhaka, Bangladesh</p>
                                 <p className="p-0 m-0">
-                                    Work for money and design for love!
+                                    sabbir.mohammad.sami@gmail.com
                                 </p>
-                                <p>I'am Sami, an Web Developer</p>
+                                <p>+880 19 7070 6676</p>
                             </div>
                             <div className="py-lg-4 py-2">
                                 <p className="pb-lg-4 text-muted">
-                                    <small>BIOGRAPHY</small>
+                                    <small>SERVICES</small>
                                 </p>
                                 <p className="p-0 m-0">
                                     Work for money and design for love!
@@ -50,7 +51,49 @@ const Hero = () => {
                             <img className="w-100" src={profile} alt="" />
                         </div>
                     </div>
-                    <div className="col-lg-3"></div>
+                    <div className="col-lg-3">
+                        <div className="text-lg-end text-start">
+                            <div className="py-lg-5">
+                                <div className="py-lg-4 py-2">
+                                    <p className="pb-lg-4 text-muted">
+                                        <small>Expertise:</small>
+                                    </p>
+                                    <p className="p-0 m-0">
+                                        HTML, CSS, ReactJS, JavaScript - ES6,
+                                    </p>
+                                    <p>
+                                        Bootstrap 5, Tailwind CSS, Swiper JS,
+                                        Daisy UI.
+                                    </p>
+                                </div>
+                                <div className="py-lg-4">
+                                    <p className="pb-lg-4 text-muted">
+                                        <small>Comfortable:</small>
+                                    </p>
+                                    <p className="p-0 m-0">
+                                        React hooq form, React Toastify,
+                                    </p>
+                                    <p className="p-0 m-0">
+                                        React Router, Firebase Authentication,
+                                        React Firebase Hooks,
+                                    </p>
+                                </div>
+                                <div className="py-lg-4 py-2">
+                                    <p className="pb-lg-4 text-muted">
+                                        <small>Familiar:</small>
+                                    </p>
+                                    <p className="p-0 m-0">
+                                        NodeJS, ExpressJS, MongoDB, CRUD
+                                        Operation, React Hook Form, JWT,
+                                    </p>
+                                    <p>
+                                        Stripe, Axios, React Query, Rechart,
+                                        etc.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
