e8c802e97d1d3079f18e060dae27671b6f540e31
SabbirSami <smd71430@gmail.com>
2022-06-09 18:43:57 +0600

    added resume

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -9,9 +9,14 @@ const Hero = () => {
                 <div className="py-5 text-center">
                     <h1 className="">Sabbir Mohammad Sami</h1>
                     <h1>Junior Full Stack Web Developer</h1>
-                    <Link to="/" className="fs-2 nav-link pt-0">
+                    <a
+                        href="./Sabbir-Mohammad-Sami-Resume.pdf"
+                        title=""
+                        download
+                        className="fs-2 nav-link pt-0"
+                    >
                         Download Resume
-                    </Link>
+                    </a>
                 </div>
                 <div className="row">
                     <div className="col-lg-3">
