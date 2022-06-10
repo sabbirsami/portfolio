a1ba0e8248175aafe169bbfaa89b835c097dac92
SabbirSami <smd71430@gmail.com>
2022-06-10 12:03:24 +0600

    about me section done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import About from "./About";
 import Footer from "./Footer";
 import HeaderNav from "./HeaderNav";
 import Hero from "./Hero";
@@ -9,6 +10,7 @@ const Home = () => {
         <div>
             <HeaderNav />
             <Hero />
+            <About />
             <Works />
             <Footer></Footer>
         </div>
