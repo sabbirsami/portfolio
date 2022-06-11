7e55ac199afbac590270a68f86ede3d01d8957d2
SabbirSami <smd71430@gmail.com>
2022-06-11 10:00:22 +0600

    Influencer Products detail section done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import SamiIndustry from "./Components/Projects/SamiIndustry";
 import FarmiOrganic from "./Components/Projects/FarmiOrganic";
 import ToDoNotes from "./Components/Projects/ToDoNotes";
 import GoldenConventionCenter from "./Components/Projects/GoldenConventionCenter";
+import InfluencerProducts from "./Components/Projects/InfluencerProducts";
 
 function App() {
     return (
@@ -30,6 +31,10 @@ function App() {
                 <Route
                     path="/works/golden-convention-center"
                     element={<GoldenConventionCenter></GoldenConventionCenter>}
+                ></Route>
+                <Route
+                    path="/works/influencer-products"
+                    element={<InfluencerProducts></InfluencerProducts>}
                 ></Route>
                 <Route path="/contact" element={<Contact></Contact>}></Route>
             </Routes>
---
+++ src/Components/Projects/InfluencerProducts.js
@@ -0,0 +1,89 @@
+import React from "react";
+import HeaderNav from "../HeaderNav";
+import project5 from "../img/project5.jpg";
+
+const InfluencerProducts = () => {
+    return (
+        <div>
+            <HeaderNav></HeaderNav>
+            <div className="container">
+                <div className="py-5">
+                    <div className="row">
+                        <div className="col-lg-5">
+                            <div className="py-3">
+                                <img
+                                    className="w-100 rounded-3"
+                                    src={project5}
+                                    alt=""
+                                />
+                            </div>
+                            <div>
+                                <span class="badge  me-2 mt-2 text-dark ">
+                                    Technologies Used:
+                                </span>
+                                <span class="badge border border-1 me-2 mt-2 text-dark ">
+                                    HTML5
+                                </span>
+                                <span class="badge border border-1 me-2 mt-2 text-dark ">
+                                    CSS3
+                                </span>
+                                <span class="badge border border-1 me-2 mt-2 text-dark ">
+                                    Bootstrap
+                                </span>
+                            </div>
+                        </div>
+                        <div className="col-lg-6">
+                            <div className="py-3 px-2">
+                                <h5 className="fw-bold">Influencer Products</h5>
+                                <p>This website is not fully done.</p>
+                                {/* <h6>Overview:</h6>
+                                <p>
+                                    <ul>
+                                        <li className="pb-2">
+                                            Implemented Email and Google login
+                                            system via Google Firebase.
+                                        </li>
+                                        <li className="pb-2">
+                                            On this website, if users wanted to
+                                            save their notes they can easily log
+                                            in using email or google. If any
+                                            user note login notes will save
+                                            temporary
+                                        </li>
+                                        <li className="pb-2">
+                                            Users can also delete their note if
+                                            user want.
+                                        </li>
+                                    </ul>
+                                </p> */}
+                                <button className="btn btn-white ">
+                                    <a
+                                        className="text-success text-decoration-none"
+                                        href="https://sabbirsami.github.io/assignment02/index.html"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Live Site
+                                    </a>
+                                </button>
+                                |
+                                <button className="btn btn-white text-success">
+                                    <a
+                                        className="text-success text-decoration-none"
+                                        href="https://github.com/sabbirsami/assignment02"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Client Site
+                                    </a>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default InfluencerProducts;
